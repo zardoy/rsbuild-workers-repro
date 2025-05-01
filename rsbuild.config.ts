e10056b4871d0f50c62cd5c3efaba412954170f5
Vitaly Turovsky <vital2580@icloud.com>
2025-05-01 13:14:02 +0300

    sp file test

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -5,13 +5,11 @@ const SINGLE_HTML_BUILD = globalThis.process.env.SINGLE_HTML_BUILD === 'true'
 export default defineConfig({
     output: {
         assetPrefix: './',
+        polyfill: 'usage',
+
         inlineScripts: SINGLE_HTML_BUILD,
         inlineStyles: SINGLE_HTML_BUILD,
         dataUriLimit: SINGLE_HTML_BUILD ? 1 * 1024 * 1024 * 1024 : undefined,
-        polyfill: 'usage',
-    },
-    html: {
-        template: SINGLE_HTML_BUILD ? './src/index.html' : undefined,
     },
     tools: {
         rspack(config) {
