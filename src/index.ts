f4b2513eb84cb220cfa62964be01580added7282
Vitaly Turovsky <vital2580@icloud.com>
2025-05-03 22:36:46 +0300

    Revert "use support by rsbuild pattern so single file build works fine"
    This reverts commit f1df323d471ce6bb3de7088401ca4edc34e7fc5d.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import Worker from './comp.worker.ts'
-
 console.log('Starting worker...')
-const worker = new Worker()
+const worker = new Worker(new URL('./comp.worker.ts', import.meta.url))
 
 worker.onmessage = (event) => {
     console.log(`In main thread: ${event.data}`)
